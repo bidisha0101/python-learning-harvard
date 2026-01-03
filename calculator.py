7f34fd917d48f1d7da3299b269f5976fe6a85cc2
bidisha0101 <bidishapanja01@gmail.com>
2026-01-03 12:42:24 +0530

    Variables and functions

---
+++ calculator.py
@@ -0,0 +1,8 @@
+x = input("whats x? ")
+y = input("whats y? ")
+z = float(input("whats z? "))  
+
+
+z = float(x) + float(y) + z
+
+print(f"{round(z):,}")
---
+++ hello.py
@@ -0,0 +1,6 @@
+# I/0
+
+name = input("What is your name?")
+# return values variables
+
+print("Hello" , name.strip().title())
